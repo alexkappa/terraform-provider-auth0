f7e39c6f3871a421f968912ec335fc8738fd9bc6
Dan Rumney <dan.rumney@ohanapediatrics.com>
2018-05-28 16:49:03 -0500

    Resolves #5: Don't send 'identifier' on Resource Server update

--- auth0/resource_auth0_resource_server.go
+++ auth0/resource_auth0_resource_server.go
@@ -94,6 +94,7 @@ func readResourceServer(d *schema.ResourceData, m interface{}) error {
 
 func updateResourceServer(d *schema.ResourceData, m interface{}) error {
 	s := buildResourceServer(d)
+	s.Identifier=""
 	api := m.(*management.Management)
 	err := api.ResourceServer.Update(d.Id(), s)
 	if err != nil {
