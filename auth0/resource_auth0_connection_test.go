967251d81726b5b568c6726c98d439ee58172b88
Fabrizio Moscon <mosconfabrizio@gmail.com>
2018-05-31 08:38:32 +0100

    add connection waad options

--- auth0/resource_auth0_connection_test.go
+++ auth0/resource_auth0_connection_test.go
@@ -18,7 +18,7 @@ func TestAccConnection(t *testing.T) {
 				Config: testAccConnectionConfig,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("auth0_connection.my_connection", "name", "Acceptance-Test-Connection"),
-					resource.TestCheckResourceAttr("auth0_connection.my_connection", "strategy", "auth0"),
+					resource.TestCheckResourceAttr("auth0_connection.my_connection", "strategy", "waad"),
 				),
 			},
 		},
@@ -26,13 +26,30 @@ func TestAccConnection(t *testing.T) {
 }
 
 const testAccConnectionConfig = `
-provider "auth0" {}
+provider "auth0" {
+	domain = ""
+	client_id = ""
+	client_secret = ""
+}
 
 resource "auth0_connection" "my_connection" {
-	name = "Acceptance-Test-Connection"
-	strategy = "auth0"
-	options = {
-		password_policy = "fair"
+	name     = "Acceptance-Test-Connection"
+  	strategy = "waad"
+  	options = {
+    	client_id     = "123456"
+    	client_secret = "123456"
+    	tenant_domain = "example.com"
+		domain_aliases = [
+			"example.io",
+		]
+		use_wsfed            = false
+		waad_protocol        = "openid-connect"
+		waad_common_endpoint = false
+		app_domain       = "my-auth0-app.eu.auth0.com"
+		api_enable_users = true
+		basic_profile    = true
+		ext_groups       = true
+		ext_profile      = true
 	}
 }
 `
