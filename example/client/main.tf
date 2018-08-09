4bfa29f05291a42966b8101b9df32d7d4b634c41
Alex Kalyvitis <alex.kalyvitis@gmail.com>
2018-08-09 16:58:33 +0200

    add example

--- example/client/main.tf
+++ example/client/main.tf
@@ -9,11 +9,22 @@ resource "auth0_client" "my_app_client" {
   callbacks       = ["https://example.com/callback"]
   allowed_origins = ["https://example.com"]
   web_origins     = ["https://example.com"]
-  grant_types =  [ "authorization_code", "http://auth0.com/oauth/grant-type/password-realm", "implicit", "password", "refresh_token" ]  
+  grant_types =  [ "authorization_code", "http://auth0.com/oauth/grant-type/password-realm", "implicit", "password", "refresh_token" ]
 
   jwt_configuration = {
     lifetime_in_seconds = 120
     secret_encoded      = true
     alg                 = "RS256"
+  }
+
+  custom_login_page_on = "true"
+
+  addons = {
+    firebase = {
+      client_email = "wer"
+      lifetime_in_seconds = 1
+      private_key = "wer"
+      private_key_id = "qwreerwerwe"
+    }
   }
 }
