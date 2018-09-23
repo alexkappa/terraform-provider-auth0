fbc76ea36fd2ca4fddf8dc910f290facff184f9f
Alex Kalyvitis <alex.kalyvitis@gmail.com>
2018-09-23 23:05:22 +0200

    improve support for custom scripts

--- example/connection/main.tf
+++ example/connection/main.tf
@@ -5,5 +5,19 @@ resource "auth0_connection" "my_connection" {
   strategy = "auth0"
   options = {
     password_policy = "excellent"
+    brute_force_protection = "true"
+    enabled_database_customization = "true"
+    custom_scripts = {
+      get_user = <<EOF
+function getByEmail (email, callback) {
+  return callback(new Error("Whoops!"))
+}
+EOF
+    }
+
+    configuration = {
+      foo = "bar"
+      bar = "baz"
+    }
   }
 }
