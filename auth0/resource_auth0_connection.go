7deb049dc72c6c69e400e7e00e12c926892c0329
Alex Kalyvitis <alex.kalyvitis@gmail.com>
2018-09-24 10:05:07 +0200

    make options optional

--- auth0/resource_auth0_connection.go
+++ auth0/resource_auth0_connection.go
@@ -50,6 +50,7 @@ func newConnection() *schema.Resource {
 			"options": {
 				Type:     schema.TypeList,
 				Optional: true,
+				Computed: true,
 				MaxItems: 1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
