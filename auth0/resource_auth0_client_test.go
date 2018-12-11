124289fd379a568d92d53f20e49d2bc45caa6181
anton mednonogov <mednonogov.anton@gmail.com>
2018-12-11 18:24:24 +0700

    Fix the failing test

--- auth0/resource_auth0_client_test.go
+++ auth0/resource_auth0_client_test.go
@@ -19,6 +19,7 @@ func TestAccClient(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("auth0_client.my_client", "name", "Application - Acceptance Test"),
 					resource.TestCheckResourceAttr("auth0_client.my_client", "is_token_endpoint_ip_header_trusted", "true"),
+					resource.TestCheckResourceAttr("auth0_client.my_client", "token_endpoint_auth_method", "client_secret_post"),
 					resource.TestCheckResourceAttr("auth0_client.my_client", "addons.0.firebase.client_email", "john.doe@example.com"),
 					resource.TestCheckResourceAttr("auth0_client.my_client", "addons.0.firebase.lifetime_in_seconds", "1"),
 					resource.TestCheckResourceAttr("auth0_client.my_client", "addons.0.samlp.0.audience", "https://example.com/saml"),
@@ -41,6 +42,7 @@ resource "auth0_client" "my_client" {
   custom_login_page_on = true
   is_first_party = true
   is_token_endpoint_ip_header_trusted = true
+  token_endpoint_auth_method = "client_secret_post"
   oidc_conformant = false
   callbacks = [ "https://example.com/callback" ]
   allowed_origins = [ "https://example.com" ]
