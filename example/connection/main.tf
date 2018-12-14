e025b6f2272e22775a58561a6100c59c56edbc54
Martin Disibio <mdisibio@gmail.com>
2018-12-14 03:46:48 -0800

    Add Waad Options (updated) (#60)
    * Add support for Waad options, fix acceptance test for valid waad domain
    
    * Add example waad connection with valid domain

--- example/connection/main.tf
+++ example/connection/main.tf
@@ -25,3 +25,28 @@ EOF
     }
   }
 }
+
+resource "auth0_connection" "my_waad_connection" {
+   name     = "my-waad-connection"
+   strategy = "waad"
+
+   options = {
+     client_id     = "1234"
+     client_secret = "1234"
+     tenant_domain = "exmaple.onmicrosoft.com"
+
+     domain_aliases = [
+       "example.io",
+     ]
+
+     use_wsfed            = false
+     waad_protocol        = "openid-connect"
+     waad_common_endpoint = false
+
+     app_domain       = "my-auth0-app.eu.auth0.com"
+     api_enable_users = true
+     basic_profile    = true
+     ext_groups       = true
+     ext_profile      = true
+   }
+ }
