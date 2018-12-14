f72a2d3e4fad41509e14f580a0fb69364f9ba16b
Alex Kalyvitis <alex.kalyvitis@gmail.com>
2018-12-14 16:22:23 +0100

    Antham add user resource (#53)
    * Add user resource
    
    * Apply gofmt -s -w
    
    * Fix error when requesting user api
    
    * improvements over initial pr
    
    * Revert "Apply gofmt -s -w"
    
    This reverts commit ee35e15b5e628fdc22e554017e22b59c253e3cba.
    
    * change api.User.Create to api.User.Update
    
    * Add user resource
    
    * Apply gofmt -s -w
    
    * Fix error when requesting user api
    
    * improvements over initial pr
    
    * Revert "Apply gofmt -s -w"
    
    This reverts commit ee35e15b5e628fdc22e554017e22b59c253e3cba.
    
    * change api.User.Create to api.User.Update
    
    * support connection conditions when updating users
    
    * change comment as we're not using state
    
    * change app_metadata and user_metadata to json strings

---
+++ auth0/resource_auth0_user.go
@@ -0,0 +1,166 @@
+package auth0
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/structure"
+	"github.com/hashicorp/terraform/helper/validation"
+	auth0 "github.com/yieldr/go-auth0"
+	"github.com/yieldr/go-auth0/management"
+)
+
+func newUser() *schema.Resource {
+	return &schema.Resource{
+		Create: createUser,
+		Read:   readUser,
+		Update: updateUser,
+		Delete: deleteUser,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"user_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
+					return old == "auth0|"+new
+				},
+			},
+			"connection_name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"email": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"username": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"password": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"phone_number": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"user_metadata": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.ValidateJsonString,
+			},
+			"email_verified": {
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"verify_email": {
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"phone_verified": {
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"app_metadata": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.ValidateJsonString,
+			},
+		},
+	}
+}
+
+func readUser(d *schema.ResourceData, m interface{}) error {
+	api := m.(*management.Management)
+	u, err := api.User.Read(d.Id())
+	if err != nil {
+		return err
+	}
+
+	d.Set("user_id", u.ID)
+	d.Set("username", u.Username)
+	d.Set("phone_number", u.PhoneNumber)
+	d.Set("email_verified", u.EmailVerified)
+	d.Set("phone_verified", u.PhoneVerified)
+	d.Set("verify_email", u.VerifyEmail)
+	d.Set("email", u.Email)
+
+	if userMeta, err := structure.FlattenJsonToString(u.UserMetadata); err != nil {
+		d.Set("user_metadata", userMeta)
+	}
+
+	if appMeta, err := structure.FlattenJsonToString(u.AppMetadata); err != nil {
+		d.Set("app_metadata", appMeta)
+	}
+
+	return nil
+}
+
+func createUser(d *schema.ResourceData, m interface{}) error {
+	u := buildUser(d)
+	api := m.(*management.Management)
+	if err := api.User.Create(u); err != nil {
+		return err
+	}
+	d.SetId(*u.ID)
+	return nil
+}
+
+func updateUser(d *schema.ResourceData, m interface{}) error {
+	u := buildUser(d)
+	api := m.(*management.Management)
+	if err := api.User.Update(d.Id(), u); err != nil {
+		return err
+	}
+	return readUser(d, m)
+}
+
+func deleteUser(d *schema.ResourceData, m interface{}) error {
+	api := m.(*management.Management)
+	return api.User.Delete(d.Id())
+}
+
+func buildUser(d *schema.ResourceData) *management.User {
+	u := &management.User{
+		ID:            String(d, "user_id"),
+		Connection:    String(d, "connection_name"),
+		Username:      String(d, "username"),
+		PhoneNumber:   String(d, "phone_number"),
+		EmailVerified: Bool(d, "email_verified"),
+		VerifyEmail:   Bool(d, "verify_email"),
+		PhoneVerified: Bool(d, "phone_verified"),
+		Email:         String(d, "email"),
+		Password:      String(d, "password"),
+	}
+
+	if d.HasChange("user_metadata") {
+		userMeta, err := structure.ExpandJsonFromString(d.Get("user_metadata").(string))
+		if err != nil {
+			u.UserMetadata = userMeta
+		}
+	}
+
+	if d.HasChange("app_metadata") {
+		appMeta, err := structure.ExpandJsonFromString(d.Get("app_metadata").(string))
+		if err != nil {
+			u.AppMetadata = appMeta
+		}
+	}
+
+	if u.Username != nil || u.Password != nil || u.EmailVerified != nil || u.PhoneVerified != nil {
+		// When updating email_verified, phone_verified, username or password
+		// we need to specify the connection property too.
+		//
+		// https://auth0.com/docs/api/management/v2#!/Users/patch_users_by_id
+		//
+		// As the builtin String function internally checks if the key has been
+		// changed, we retrieve the value of "connection_name" regardless of
+		// change.
+		u.Connection = auth0.String(d.Get("connection_name").(string))
+	}
+
+	return u
+}
---
+++ auth0/resource_auth0_user_test.go
@@ -0,0 +1,71 @@
+package auth0
+
+import (
+	"regexp"
+
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccUserMissingRequiredParams(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		Providers: map[string]terraform.ResourceProvider{
+			"auth0": Provider(),
+		},
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccUserMissingRequiredParam,
+				ExpectError: regexp.MustCompile(`config is invalid: auth0_user.user: "connection_name": required field is not set`),
+			},
+		},
+	})
+}
+
+const testAccUserMissingRequiredParam = `
+provider "auth0" {}
+
+resource "auth0_user" "user" {}
+`
+
+func TestAccUserCreateUser(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		Providers: map[string]terraform.ResourceProvider{
+			"auth0": Provider(),
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testAccUserCreateUser,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("auth0_user.user", "user_id", "12345"),
+					resource.TestCheckResourceAttr("auth0_user.user", "email", "test@test.com"),
+					resource.TestCheckResourceAttr("auth0_user.user", "connection_name", "Username-Password-Authentication"),
+				),
+			},
+		},
+	})
+}
+
+const testAccUserCreateUser = `
+provider "auth0" {}
+
+resource "auth0_user" "user" {
+  connection_name = "Username-Password-Authentication"
+  user_id = "12345"
+  email = "test@test.com"
+  password = "testtest$12$12"
+  user_metadata = <<EOF
+{
+  	"foo": "bar",
+  	"bar": { "baz": "qux" }
+}
+EOF
+  app_metadata = <<EOF
+{
+  	"foo": "bar",
+  	"bar": { "baz": "qux" }
+}
+EOF
+}
+`
