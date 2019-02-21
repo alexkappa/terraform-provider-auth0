fdf91f4831bbf058afb1bbbade0fa0a08b87e16b
Omar Khudeira <omar@users.noreply.github.com>
2019-02-21 13:18:04 -0600

    Update flags to TypeMap and set `flag` properties to computed to handle situation where the flag is added in the Terraform then removed later where it is still returned with the previous value.

--- auth0/resource_auth0_tenant.go
+++ auth0/resource_auth0_tenant.go
@@ -81,39 +81,40 @@ func newTenant() *schema.Resource {
 				},
 			},
 			"flags": {
-				Type:     schema.TypeMap,
+				Type:     schema.TypeList,
 				Optional: true,
+				MaxItems: 1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"change_pwd_flow_v1": {
 							Type:     schema.TypeString,
 							Optional: true,
-							Default:  false,
+							Computed: true,
 						},
 						"enable_client_connections": {
 							Type:     schema.TypeString,
 							Optional: true,
-							Default:  true,
+							Computed: true,
 						},
 						"enable_apis_section": {
 							Type:     schema.TypeString,
 							Optional: true,
-							Default:  true,
+							Computed: true,
 						},
 						"enable_pipeline2": {
 							Type:     schema.TypeString,
 							Optional: true,
-							Default:  true,
+							Computed: true,
 						},
 						"enable_dynamic_client_registration": {
 							Type:     schema.TypeString,
 							Optional: true,
-							Default:  false,
+							Computed: true,
 						},
 						"enable_custom_domain_in_emails": {
 							Type:     schema.TypeString,
 							Optional: true,
-							Default:  false,
+							Computed: true,
 						},
 					},
 				},
@@ -142,6 +143,11 @@ func newTenant() *schema.Resource {
 			"session_lifetime": {
 				Type:     schema.TypeInt,
 				Optional: true,
+			},
+			"sandbox_version": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
 			},
 		},
 	}
@@ -172,6 +178,7 @@ func readTenant(d *schema.ResourceData, m interface{}) error {
 	d.Set("support_url", t.SupportURL)
 	d.Set("allowed_logout_urls", t.AllowedLogoutURLs)
 	d.Set("session_lifetime", t.SessionLifetime)
+	d.Set("sandbox_version", t.SandboxVersion)
 
 	return nil
 }
@@ -200,6 +207,7 @@ func buildTenant(d *schema.ResourceData) *management.Tenant {
 		SupportURL:        String(d, "support_url"),
 		AllowedLogoutURLs: Slice(d, "allowed_logout_urls"),
 		SessionLifetime:   Int(d, "session_lifetime"),
+		SandboxVersion:    String(d, "sandbox_version"),
 	}
 
 	List(d, "change_password").First(func(v interface{}) {
@@ -220,16 +228,17 @@ func buildTenant(d *schema.ResourceData) *management.Tenant {
 		}
 	})
 
-	m := Map(d, "flags")
-
-	t.Flags = &management.TenantFlags{
-		ChangePasswordFlowV1:            Bool(MapData(m), "change_pwd_flow_v1"),
-		EnableClientConnections:         Bool(MapData(m), "enable_client_connections"),
-		EnableAPIsSection:               Bool(MapData(m), "enable_apis_section"),
-		EnablePipeline2:                 Bool(MapData(m), "enable_pipeline2"),
-		EnableDynamicClientRegistration: Bool(MapData(m), "enable_dynamic_client_registration"),
-		EnableCustomDomainInEmails:      Bool(MapData(m), "enable_custom_domain_in_emails"),
-	}
+	List(d, "flags").First(func(v interface{}) {
+		m := v.(map[string]interface{})
+		t.Flags = &management.TenantFlags{
+			ChangePasswordFlowV1:            Bool(MapData(m), "change_pwd_flow_v1"),
+			EnableClientConnections:         Bool(MapData(m), "enable_client_connections"),
+			EnableAPIsSection:               Bool(MapData(m), "enable_apis_section"),
+			EnablePipeline2:                 Bool(MapData(m), "enable_pipeline2"),
+			EnableDynamicClientRegistration: Bool(MapData(m), "enable_dynamic_client_registration"),
+			EnableCustomDomainInEmails:      Bool(MapData(m), "enable_custom_domain_in_emails"),
+		}
+	})
 
 	return t
 }
