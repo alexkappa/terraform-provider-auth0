5dcc9f6f9e3afbd9b998cf554ba63b5a1d8b2e90
Thilo Keber <thilo.keber@b3i.tech>
2019-06-05 17:25:07 +0200

    Add support for user attribute `nickname`.

--- example/user/main.tf
+++ example/user/main.tf
@@ -3,7 +3,8 @@ provider "auth0" {}
 resource "auth0_user" "user" {
   connection_name = "Username-Password-Authentication"
   user_id = "12345"
+  nickname = "testnick"
   email = "test@test.com"
   email_verified = true
   password = "testtest$12$12"
-}
+:}
