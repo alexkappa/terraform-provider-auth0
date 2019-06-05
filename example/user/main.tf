1d04198034e64daeff5f0f02c92748e138f886e0
Thilo Keber <thilo.keber@b3i.tech>
2019-06-05 17:41:30 +0200

    fix user example (type, username is required property)

--- example/user/main.tf
+++ example/user/main.tf
@@ -3,8 +3,9 @@ provider "auth0" {}
 resource "auth0_user" "user" {
   connection_name = "Username-Password-Authentication"
   user_id = "12345"
+  username = "test"
   nickname = "testnick"
   email = "test@test.com"
   email_verified = true
-  password = "testtest$12$12"
-:}
+  password = "passpass$12$12"
+}
