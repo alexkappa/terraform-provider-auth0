b73b6c28b0f07a07cd12034484fa81bbf61d4ebb
Stephen Bishtawi <stephen.bishtawi@originate.com>
2019-09-24 10:44:53 -0700

    Fix

--- auth0/resource_auth0_connection.go
+++ auth0/resource_auth0_connection.go
@@ -494,7 +494,7 @@ func buildConnection(d *schema.ResourceData) *management.Connection {
 
 			c.Options.PasswordDictionary = make(map[string]interface{})
 			c.Options.PasswordDictionary["enable"] = Bool(MapData(m), "enable")
-			c.Options.PasswordDictionary["dictionary"] = Slice(MapData(m), "dictionary")
+			c.Options.PasswordDictionary["dictionary"] = Set(MapData(m), "dictionary").Slice()
 		})
 	})
 
