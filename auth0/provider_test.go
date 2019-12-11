83892a3d6ab1e971c75c99ec46a2a0cbe4655725
Alex Kalyvitis <alex.kalyvitis@gmail.com>
2019-12-11 23:50:04 +0100

    make provider debug tests more concise

--- auth0/provider_test.go
+++ auth0/provider_test.go
@@ -12,54 +12,29 @@ func TestProvider(t *testing.T) {
 }
 
 func TestProvider_debugDefaults(t *testing.T) {
-	testCases := map[string]struct {
-		envSet   bool
-		envValue string
-		expected bool
-	}{
-		`should be true when AUTH0_DEBUG environment variable is set to "1"`: {
-			envSet:   true,
-			envValue: "1",
-			expected: true,
-		},
-		`should be true when AUTH0_DEBUG environment variable is set to "true"`: {
-			envSet:   true,
-			envValue: "true",
-			expected: true,
-		},
-		`should be true when AUTH0_DEBUG environment variable is set to "on"`: {
-			envSet:   true,
-			envValue: "on",
-			expected: true,
-		},
-		`should be false when AUTH0_DEBUG environment variable is set to "false"`: {
-			envSet:   true,
-			envValue: "false",
-			expected: false,
-		},
-		"should be false if no AUTH0_DEBUG environment variable is defined": {
-			envSet:   false,
-			expected: false,
-		},
-	}
+	for value, expected := range map[string]bool{
+		"1":     true,
+		"true":  true,
+		"on":    true,
+		"0":     false,
+		"off":   false,
+		"false": false,
+		"foo":   false,
+		"":      false,
+	} {
+		os.Unsetenv("AUTH0_DEBUG")
+		if value != "" {
+			os.Setenv("AUTH0_DEBUG", value)
+		}
 
-	for name, tc := range testCases {
-		t.Run(name, func(t *testing.T) {
-			os.Unsetenv("AUTH0_DEBUG")
-			if tc.envSet {
-				os.Setenv("AUTH0_DEBUG", tc.envValue)
-			}
+		p := Provider()
+		debug, err := p.Schema["debug"].DefaultValue()
+		if err != nil {
+			t.Fatalf("Unexpected error: %v", err)
+		}
 
-			p := Provider()
-
-			debug, err := p.Schema["debug"].DefaultValue()
-			if err != nil {
-				t.Fatalf("Unexpected error: %v", err)
-			}
-
-			if debug.(bool) != tc.expected {
-				t.Fatalf("Expected debug to be %v, but got %v", tc.expected, debug)
-			}
-		})
+		if debug.(bool) != expected {
+			t.Fatalf("Expected debug to be %v, but got %v", expected, debug)
+		}
 	}
 }
