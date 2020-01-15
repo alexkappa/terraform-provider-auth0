79da490af01f774b3705efac0f501c00fae74d6a
Alex Kalyvitis <alex.kalyvitis@gmail.com>
2020-01-15 11:52:09 +0200

    add client_grant test case to verify empty scopes

--- auth0/resource_auth0_client_grant_test.go
+++ auth0/resource_auth0_client_grant_test.go
@@ -15,17 +15,23 @@ func TestAccClientGrant(t *testing.T) {
 		},
 		Steps: []resource.TestStep{
 			{
-				Config: testAccClientGrantConfig,
+				Config: testAccClientGrantConfigCreate,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("auth0_client_grant.my_client_grant", "audience", "https://api.example.com/client-grant-test"),
 					resource.TestCheckResourceAttr("auth0_client_grant.my_client_grant", "scope.0", "create:foo"),
+				),
+			},
+			{
+				Config: testAccClientGrantConfigUpdate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("auth0_client_grant.my_client_grant", "scope.#", "0"),
 				),
 			},
 		},
 	})
 }
 
-const testAccClientGrantConfig = `
+const testAccClientGrantConfigCreate = `
 provider "auth0" {}
 
 resource "auth0_client" "my_client" {
@@ -51,5 +57,34 @@ resource "auth0_client_grant" "my_client_grant" {
   client_id = "${auth0_client.my_client.id}"
   audience = "${auth0_resource_server.my_resource_server.identifier}"
   scope = [ "create:foo" ]
+}
+`
+
+const testAccClientGrantConfigUpdate = `
+provider "auth0" {}
+
+resource "auth0_client" "my_client" {
+  name = "Application - Client Grant - Acceptance Test"
+  custom_login_page_on = true
+  is_first_party = true
+}
+
+resource "auth0_resource_server" "my_resource_server" {
+  name = "Resource Server - Client Grant - Acceptance Test"
+  identifier = "https://api.example.com/client-grant-test"
+  scopes {
+       value = "create:foo"
+       description = "Create foos"
+  }
+  scopes {
+       value = "create:bar"
+       description = "Create bars"
+  }
+}
+
+resource "auth0_client_grant" "my_client_grant" {
+  client_id = "${auth0_client.my_client.id}"
+  audience = "${auth0_resource_server.my_resource_server.identifier}"
+  scope = [ ] # empty scope
 }
 `
