7ca3b6e7073aa10bc12585cca4cd60b0868c1173
Chris Griggs <cgriggs@hashicorp.com>
2020-01-29 08:20:41 -0800

    fmt main.go

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/terraform-providers/terraform-provider-auth0/auth0"
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/terraform-providers/terraform-provider-auth0/auth0"
 )
 
 func main() {
