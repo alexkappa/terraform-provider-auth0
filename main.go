199102a8a1ca2169a8e6aeafc3d135f68716587e
Chris Griggs <cgriggs@hashicorp.com>
2020-01-29 08:10:41 -0800

    update source path

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/alexkappa/terraform-provider-auth0/auth0"
+	"github.com/terraform-providers/terraform-provider-auth0/auth0"
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
