591a7f7d8b580eb1770421b954a926aaa00ca9a4
Alex Kalyvitis <alex.kalyvitis@gmail.com>
2020-04-06 12:06:45 +0200

    add conditional to json

--- auth0/resource_auth0_tenant_test.go
+++ auth0/resource_auth0_tenant_test.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
-	"github.com/terraform-providers/terraform-provider-auth0/auth0/internal/debug"
 )
 
 func TestAccTenant(t *testing.T) {
@@ -45,7 +44,6 @@ func TestAccTenant(t *testing.T) {
 					resource.TestCheckResourceAttr("auth0_tenant.my_tenant", "universal_login.0.colors.0.primary", "#0059d6"),
 					resource.TestCheckResourceAttr("auth0_tenant.my_tenant", "universal_login.0.colors.0.page_background", "#000000"),
 					resource.TestCheckResourceAttr("auth0_tenant.my_tenant", "default_redirection_uri", "https://example.com/login"),
-					debug.DumpAttr("auth0_tenant.my_tenant"),
 				),
 			},
 			// This test case confirms issue #160 where boolean values from a
