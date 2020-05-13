6214c224803f5cb75698b1f074a2dbc61e5e9901
Alex Kalyvitis <alex.kalyvitis@gmail.com>
2020-05-13 09:34:32 +0200

    fix tests

--- auth0/resource_auth0_connection_test.go
+++ auth0/resource_auth0_connection_test.go
@@ -951,38 +951,52 @@ resource "auth0_connection" "my_connection" {
 func TestConnectionInstanceStateUpgradeV0(t *testing.T) {
 
 	for _, tt := range []struct {
-		name     string
-		state    map[string]interface{}
-		expected map[string]interface{}
+		name            string
+		version         interface{}
+		versionExpected int
 	}{
 		{
-			name:     "Empty",
-			state:    map[string]interface{}{"strategy_version": ""},
-			expected: map[string]interface{}{"strategy_version": 0},
+			name:            "Empty",
+			version:         "",
+			versionExpected: 0,
 		},
 		{
-			name:     "Zero",
-			state:    map[string]interface{}{"strategy_version": "0"},
-			expected: map[string]interface{}{"strategy_version": 0},
+			name:            "Zero",
+			version:         "0",
+			versionExpected: 0,
 		},
 		{
-			name:     "NonZero",
-			state:    map[string]interface{}{"strategy_version": "123"},
-			expected: map[string]interface{}{"strategy_version": 123},
+			name:            "NonZero",
+			version:         "123",
+			versionExpected: 123,
 		},
 		{
-			name:     "Invalid",
-			state:    map[string]interface{}{"strategy_version": "foo"},
-			expected: map[string]interface{}{"strategy_version": 0},
+			name:            "Invalid",
+			version:         "foo",
+			versionExpected: 0,
 		},
 	} {
 		t.Run(tt.name, func(t *testing.T) {
-			actual, err := connectionSchemaUpgradeV0(tt.state, nil)
+
+			state := map[string]interface{}{
+				"options": []interface{}{
+					map[string]interface{}{"strategy_version": tt.version},
+				},
+			}
+
+			actual, err := connectionSchemaUpgradeV0(state, nil)
 			if err != nil {
 				t.Fatalf("error migrating state: %s", err)
 			}
-			if !reflect.DeepEqual(tt.expected, actual) {
-				t.Fatalf("\n\nexpected:\n\n%#v\n\ngot:\n\n%#v\n\n", tt.expected, actual)
+
+			expected := map[string]interface{}{
+				"options": []interface{}{
+					map[string]interface{}{"strategy_version": tt.versionExpected},
+				},
+			}
+
+			if !reflect.DeepEqual(expected, actual) {
+				t.Fatalf("\n\nexpected:\n\n%#v\n\ngot:\n\n%#v\n\n", expected, actual)
 			}
 		})
 	}
