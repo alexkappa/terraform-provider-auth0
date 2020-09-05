84f9725a34390132aacf6108606a8890571f9845
etwillbefine <tim.gatzemeier@gmx.de>
2020-09-05 09:58:18 +0200

    migrate validation map to validation.username

--- auth0/resource_auth0_connection_test.go
+++ auth0/resource_auth0_connection_test.go
@@ -1010,6 +1010,68 @@ func TestConnectionInstanceStateUpgradeV0(t *testing.T) {
 	}
 }
 
+func TestConnectionInstanceStateUpgradeV1(t *testing.T) {
+
+	for _, tt := range []struct {
+		name               string
+		validation         map[string]string
+		validationExpected []map[string]interface{}
+	}{
+		{
+			name: "Only Min",
+			validation: map[string]string{
+				"min": "5",
+			},
+			validationExpected: []map[string]interface{}{
+				{
+					"username": map[string]string{
+						"min": "5",
+					},
+				},
+			},
+		},
+		{
+			name: "Min and Max",
+			validation: map[string]string{
+				"min": "5",
+				"max": "10",
+			},
+			validationExpected: []map[string]interface{}{
+				{
+					"username": map[string]string{
+						"min": "5",
+						"max": "10",
+					},
+				},
+			},
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+
+			state := map[string]interface{}{
+				"options": []interface{}{
+					map[string]interface{}{"validation": tt.validation},
+				},
+			}
+
+			actual, err := connectionSchemaUpgradeV1(state, nil)
+			if err != nil {
+				t.Fatalf("error migrating state: %s", err)
+			}
+
+			expected := map[string]interface{}{
+				"options": []interface{}{
+					map[string]interface{}{"validation": tt.validationExpected},
+				},
+			}
+
+			if !reflect.DeepEqual(expected, actual) {
+				t.Fatalf("\n\nexpected:\n\n%#v\n\ngot:\n\n%#v\n\n", expected, actual)
+			}
+		})
+	}
+}
+
 func TestAccConnectionSAML(t *testing.T) {
 	rand := random.String(6)
 
