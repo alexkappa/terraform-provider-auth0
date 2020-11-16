078c69883a1dc1a90c84ebd719281636a9a27769
Alex Kalyvitis <a.kalyvitis@thebeat.co>
2020-11-16 15:08:48 +0100

    Move Not function closer to All/Any

--- auth0/resource_data.go
+++ auth0/resource_data.go
@@ -151,14 +151,6 @@ func (c Condition) Eval(d ResourceData, key string) bool {
 	return c(d, key)
 }
 
-// Not is a condition that evaluates to true if its child condition evaluates to
-// false. False otherwise.
-func Not(condition Condition) Condition {
-	return func(d ResourceData, key string) bool {
-		return !condition.Eval(d, key)
-	}
-}
-
 // IsNewResource is a condition that evaluates to true if the resource access is
 // new.
 func IsNewResource() Condition {
@@ -199,6 +191,14 @@ func All(conditions ...Condition) Condition {
 			}
 		}
 		return true
+	}
+}
+
+// Not is a condition that evaluates to true if its child condition evaluates to
+// false. False otherwise.
+func Not(condition Condition) Condition {
+	return func(d ResourceData, key string) bool {
+		return !condition.Eval(d, key)
 	}
 }
 
