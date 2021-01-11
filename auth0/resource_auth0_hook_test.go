ad7dd25eb2b5e1a2f34217c928633e9b3e1b3933
woz5999 <jeff.wozniak@umbralab.com>
2021-01-11 12:43:15 -0800

    Support hook dependencies

--- auth0/resource_auth0_hook_test.go
+++ auth0/resource_auth0_hook_test.go
@@ -18,6 +18,7 @@ func TestAccHook(t *testing.T) {
 				Config: testAccHookCreate,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("auth0_hook.my_hook", "name", "pre-user-reg-hook"),
+					resource.TestCheckResourceAttr("auth0_hook.my_hook", "dependencies.#", "0"),
 					resource.TestCheckResourceAttr("auth0_hook.my_hook", "script", "function (user, context, callback) { callback(null, { user }); }"),
 					resource.TestCheckResourceAttr("auth0_hook.my_hook", "trigger_id", "pre-user-registration"),
 					resource.TestCheckResourceAttr("auth0_hook.my_hook", "enabled", "true"),
@@ -27,6 +28,8 @@ func TestAccHook(t *testing.T) {
 				Config: testAccHookUpdate,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("auth0_hook.my_hook", "name", "pre-user-reg-hook"),
+					resource.TestCheckResourceAttr("auth0_hook.my_hook", "dependencies.#", "1"),
+					resource.TestCheckResourceAttr("auth0_hook.my_hook", "dependencies.0.auth0", "2.30.0"), // TODO figure out the right value
 					resource.TestCheckResourceAttr("auth0_hook.my_hook", "script", "function (user, context, callback) { console.log(user); callback(null, { user }); }"),
 					resource.TestCheckResourceAttr("auth0_hook.my_hook", "trigger_id", "pre-user-registration"),
 					resource.TestCheckResourceAttr("auth0_hook.my_hook", "enabled", "false"),
@@ -43,6 +46,7 @@ resource "auth0_hook" "my_hook" {
   trigger_id = "pre-user-registration"
   script = "function (user, context, callback) { callback(null, { user }); }"
   enabled = true
+	dependencies {}
 }
 `
 
@@ -53,6 +57,9 @@ resource "auth0_hook" "my_hook" {
   trigger_id = "pre-user-registration"
   script = "function (user, context, callback) { console.log(user); callback(null, { user }); }"
   enabled = false
+	dependencies {
+		auth0 = "2.30.0"
+	}
 }
 `
 
