d34e31bd6cb05fdb96cce897fdee43f915ced4d6
Alex Kalyvitis <a.kalyvitis@thebeat.co>
2021-12-01 15:38:10 +0100

    Add action example

--- example/action/main.tf
+++ example/action/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     auth0 = {
-      source = "alexkappa/auth0"
-      version = "0.24.1"
+      source  = "alexkappa/auth0"
+      version = "0.24.3"
     }
   }
 }
@@ -10,15 +10,38 @@ terraform {
 provider "auth0" {}
 
 resource "auth0_action" "do" {
-	name = "Test Action ${timestamp()}"
-	supported_triggers {
-		id = "post-login"
-		version = "v2"
-	}
-	code = <<-EOT
+
+  name = "Test Action ${timestamp()}"
+
+  supported_triggers {
+    id      = "post-login"
+    version = "v2"
+  }
+
+  runtime = "node16"
+  code    = <<-EOT
 	exports.onContinuePostLogin = async (event, api) => { 
 		console.log(event) 
 	};"
 	EOT
-	deploy = true
+
+  dependencies {
+    name    = "lodash"
+    version = "latest"
+  }
+  dependencies {
+    name    = "request"
+    version = "latest"
+  }
+
+  secrets {
+    name  = "FOO"
+    value = "Foo"
+  }
+  secrets {
+    name  = "BAR"
+    value = "Bar"
+  }
+
+  deploy = true
 }
